058133f21b85c9f61c2927ec0df681d1798960da
Sander Hakze <sanderhakze@iProvannder2126.localdomain>
2026-05-22 01:00:29 +0200

    Build rooster planning modules

--- public/index.php
+++ public/index.php
@@ -7,16 +7,28 @@ use Roostar\Core\Http\Response;
 use Roostar\Core\Http\Router;
 use Roostar\Core\Database\Connection;
 use Roostar\Core\Security\Session;
+use Roostar\Modules\Setup\DatabaseSetupController;
 
 $app = require __DIR__ . '/../bootstrap/app.php';
 
 Connection::configure($app['config']['database']);
 Session::start($app['config']['security']['session_name']);
 
+$request = Request::fromGlobals();
+$setup = new DatabaseSetupController();
+if ($setup->isSetupRequest($request)) {
+    $response = $request->method === 'POST'
+        ? $setup->store($request)
+        : ($setup->tokenIsValid($request)
+            ? $setup->show($request)
+            : Response::html('Setup token ontbreekt of is ongeldig.', 403));
+    $response->send();
+    return;
+}
+
 $router = new Router();
 (require __DIR__ . '/../routes/web.php')($router);
 
-$request = Request::fromGlobals();
 $response = $router->dispatch($request);
 
 if (!$response instanceof Response) {
